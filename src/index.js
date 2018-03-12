108c42b19f501375417ee51a685e076b031a3e26
omar.crosby <omar.crosby@gmail.com>
2018-03-12 08:59:41 -0400

    Refactoring the handler's logging and setting the max line length to 90.

--- src/index.js
+++ src/index.js
@@ -9,13 +9,17 @@ exports.handler = (event, context, callback) => {
     let division;
 
     try {
-        console.log(`Received event:\n${JSON.stringify(event)}`);
+        console.log(`Received event:\n${JSON.stringify(event, null, 2)}`);
+        console.log(`context: ${JSON.stringify(context, null, 2)}`);
+        console.log(`pathParameters: ${JSON.stringify(event.pathParameters, null, 2)}`);
 
         season = event.pathParameters.season;
         year = event.pathParameters.year;
         division = event.pathParameters.division;
 
-        console.log(`Retrieving SincSports teams in the ${division} division for the ${season} of ${year} ...`);
+        console.log(`season = ${season}`);
+        console.log(`year = ${year}`);
+        console.log(`division = ${division}`);
 
         SincSportsService.getTeams(season, year, division)
             .then((teams) => {
@@ -24,7 +28,6 @@ exports.handler = (event, context, callback) => {
             .catch((err) => {
                 responder.error(err);
             });
-
     } catch(err) {
         responder.error(err);
     }
